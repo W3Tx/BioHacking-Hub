08b5592716f129224bc16a7b03a1f6949011fa45
W3Tx <foto.martindkawan@hotmail.com>
2025-07-18 10:31:29 +0200

    Unit Test, correction and Documentary Finish

---
+++ DailyPerformanceDashboard.Tests/TaskTests.cs
@@ -0,0 +1,21 @@
+﻿using DailyPerformanceDashboard.Models;
+
+namespace DailyPerformanceDashboard.Tests
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void Should_Create_TaskItem_With_Valid_Title()
+        {
+            var task = new TaskItem
+            {
+                Title = "Tagesbericht schreiben",
+                Date = DateTime.Today,
+                IsDone = false
+            };
+
+            Assert.Equal("Tagesbericht schreiben", task.Title);
+            Assert.False(task.IsDone);
+        }
+    }
+}
